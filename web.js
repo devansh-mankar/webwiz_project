7cdc823a6218a9a9bdf555d46c6db8b655debe7e
devansh-mankar <98455928+devansh-mankar@users.noreply.github.com>
2023-01-23 23:31:46 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1,18 +1,23 @@
 setInterval(updatetime,1000)
 function updatetime(){
-    samay.innerHTML=new Date();
+    var elem= document.getElementById("heading");
 }
 
 function red(){
-    document.bgcolor="red";
+    document.body.style.backgroundColor = '#ffffff';
 
 }
 function fun(){
     alert("welcome")
 }
 function changetext(){
-    let headings=document.getElementById('heading');
-    headings.textContent='welcome!';
+    
     setTimeout(changetext,5000);
 
 }
+
+window.setInterval(function()
+{
+    let headings=document.getElementById('heading');
+    headings.textContent='welcome!';
+},5000)
